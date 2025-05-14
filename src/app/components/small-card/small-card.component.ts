bd618083e5e38c9fc5b0b4198b201846caa18453
Jeferson Braine <jefeleal363@gmail.com>
2025-05-14 15:12:11 -0300

    update: Change the content for use only inputs in the components

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class SmallCardComponent {
 
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription: string = ""
 }
